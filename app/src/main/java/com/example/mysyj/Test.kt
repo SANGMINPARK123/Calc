391cbd408a01b71e9a212140a8fc784360483819
sangmin <psm64181657@gmail.com>
2023-12-07 19:13:22 +0900

    first commit

---
+++ app/src/main/java/com/example/mysyj/Test.kt
@@ -0,0 +1,15 @@
+package com.example.mysyj
+
+fun main() {
+    var operator = '+'
+
+    if (operator === '+') {
+        // 덧셈일때
+    } else if (operator === '-') {
+
+    } else if (operator === '*') {
+
+    } else if (operator === '/') {
+    }
+
+}
